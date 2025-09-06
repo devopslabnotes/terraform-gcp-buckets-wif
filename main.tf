d0d0379a98fbb54539b1f39582b6bd9ef8bdc9d6
devopslabnotes <anuj.k@devopslabnotes.com>
2025-09-06 09:54:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "5.27.0"
     }
   }
 }
