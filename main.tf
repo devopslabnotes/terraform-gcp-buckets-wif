ae849ce4f4d331fc587b9008a560d3801a17ba47
devopslabnotes <anuj.k@devopslabnotes.com>
2025-09-06 09:43:03 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "example" {
+  name     = "my-wif-secured-bucket-${random_id.bucket_suffix.hex}"
+  location = var.region
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+variable "project_id" {}
+variable "region" {}
